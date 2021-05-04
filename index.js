5f80a8240b83048259253db6be70caa3da8f82db
Nurina <nurina.junus@gmail.com>
2021-05-03 19:50:10 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove(main);
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", 'victory');
+newHeader.innerHTML = "<h1>nurina is the champion</h1>";
